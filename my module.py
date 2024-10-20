58f4c96098caca9e9c5a664f746ac569ec437945
Vaishnavi-Manick <vaishnavim611@gmail.com>
2024-10-20 16:49:24 +0530

    Add files via upload

---
+++ my module.py
@@ -0,0 +1,16 @@
+import random
+n=random.randint(0,10)
+print(n)
+
+##import random
+#r=random.random()*10
+#print(r)
+
+#import random
+#random_float=random.uniform(0,6)
+#print(random_float)
+
+if n==0 :
+   print("heads")
+else:
+    print("tails")
